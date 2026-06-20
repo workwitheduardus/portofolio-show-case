4e3bda0670dc97f7f859d35bd80126fe3361c706
workwitheduardus <workwitheduardus@gmail.com>
2026-06-20 14:58:20 +0700

    global.css content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next";import "./globals.css";
+import { Red_Hat_Display } from "next/font/google";
+import clsx from "clsx";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const redHatDisplay = Red_Hat_Display({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ['400', '500','600','700', '800'],
+  style: ['normal', 'italic'],
+  variable: '--font-red-hat-display'
 });
 
 export const metadata: Metadata = {
@@ -19,15 +16,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className={clsx(redHatDisplay.variable, 'antialiased')}>{children}</body>
     </html>
   );
 }
